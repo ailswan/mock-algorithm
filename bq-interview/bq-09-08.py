47fd1fd1ea4d484db1d5626d21591e97e2459ccd
ailswan <ailswanster@gmail.com>
2024-09-08 12:27:58 -0400

    update

---
+++ bq-interview/bq-09-08.py
@@ -0,0 +1,54 @@
+# 1. Tell me about one of your most successful projects?
+#   -  Your role in this project?
+#   - What challenges or obstacles did you encounter?
+#(Intrinsic Motivation)
+
+# 2.Tell me about a time when someone was hard to work with?
+# - Did the issue affect your work or the team's performance? 
+# - How did you resolve the conflict or work through the difficulties?
+#(Conflict Resolution)
+
+# 3.About the previous manager?
+#(Empathy and Ability to Work with Others)
+
+#4. Tell me about a time when you received critical feedback.
+#  How did you respond and apply the feedback?
+# (Growth)
+
+#5. Describe a time when you had to present a complex idea or project
+#  to a non-technical audience. How did you ensure they understood?
+#(Communication)
+
+# 6.Describe a situation where you worked on something for an extended 
+# period without immediate results. What kept you going?
+#(Perseverance)
+
+#7.Describe a situation where you saw an opportunity to make improvements 
+# but were not specifically asked to. How did you take initiative?
+#(Ability to be Proactive and Work in an Unstructured Environment)
+
+#8.Tell me about a project or skill you developed over time. 
+# How did you grow throughout the process?
+#(Growth)
+
+
+
+# Aileen mock
+# 1. Self introduction
+
+# 2. Most memorable project?
+# Give a brief summary about the project
+# Provide details in the following sub-questions.
+# Highlight the point close to the question's theme (memorable)
+
+# 3. Conflict with sister team?
+
+# 4. Tell me more about your team
+# pick one or two focus points
+
+# 5. Competing responsibilities or priority?
+
+# 6. What wakes you up in the morning?
+#   -- what do you like most about the chatGPT integration R&D
+
+# 7. How do you communicate with various people/teams?
