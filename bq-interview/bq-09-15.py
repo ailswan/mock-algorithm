e79fa14680392482e1d79c61ba4d0ba8d5c2413b
ailswan <ailswanster@gmail.com>
2024-09-15 12:05:59 -0400

    update

---
+++ bq-interview/bq-09-15.py
@@ -0,0 +1,36 @@
+# 1. Take project to completion (ownership)
+# 1a. Was there any pushback from 3rd party devs?
+
+# 2. Tell me about a time you cared about customer satisfaction
+# 2a. Did you notice any decrease in customer churn?
+
+# 3. Disagreeing with coworkers, but still committed
+# 3a. Did your pilot work get used in production?
+
+# 4. Missed deadline, what did you do to rememdy the delay?
+# 4a. What was the result? How did the furniture company react to the delay?
+
+# 5. Talk about a time when you had to Simplify (ie: goals, requirements, implementation, etc)
+# 5a. Did they agree to the proposal of a smaller feature set?
+
+
+
+# 1. Can you describe a time when you went above and beyond to meet a customer’s needs?
+# (Customer Obsession)
+
+# 2. Describe a situation where you took full responsibility for a project or problem, even
+#  if it was outside your usual scope of work.
+# (Ownership)
+
+
+# 3. Tell me about a time when you developed a new approach to solve a complex problem.
+# (Invent & Simplify)
+
+# 4.Describe a time when you learned a new skill or technology that significantly benefited your work.
+# (Learn and Be Curious)
+
+# 5.Tell me about a situation where you had to ensure a project met exceptionally high-quality criteria.
+# (Hire and Develop the Best [person])
+# https://www.amazon.jobs/content/en/our-workplace/leadership-principles
+# Leaders raise the performance bar with every hire and promotion. They recognize exceptional talent, and willingly move them throughout the organization.
+# Leaders develop leaders and take seriously their role in coaching others. We work on behalf of our people to invent mechanisms for development like Career Choice.
