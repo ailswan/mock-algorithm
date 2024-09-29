ad7ed4d352e9faccd6219d418968051f478afb11
ailswan <ailswanster@gmail.com>
2024-09-29 14:10:27 -0400

    update

---
+++ bq-interview/bq-09-29.py
@@ -0,0 +1,32 @@
+#1. Tell me about a time when you built a feature or solved a problem that directly improved the customer experience.
+(Customer Obsession)
+
+#2. Tell me about a time when you identified a technical issue that no one else noticed, and you took full responsibility for fixing it.
+(Ownership)
+
+#3. Tell me about a complex technical challenge you simplified for your team or a customer.
+(Invent and Simplify)
+
+#4. How do you stay current with evolving technologies, and how have you applied something new you learned to a recent project?
+(Learn and Be Curious)
+
+#5. Tell me about a time when you strongly disagreed with a technical decision made by your team or manager. How did you handle it, and what was the result?
+(Have Backbone; Disagree and Commit)
+
+#6.Give an example of a project where you delivered results under tight deadlines or high pressure.
+(Deliver Results)
+
+
+
+Aileen Mock
+#1. What was the most frustrating project? How did you navigate through the project?
+
+#2. Project or time when you knew your instincts were correct and how did you convince your colleagues to follow your approach?
+
+#3. Noticed something in the code or project that needed improvement… How did you go about making that improvement?
+
+#4. Weakness
+
+#5. Time or project where there was not sufficient information, but you made a decision anyways. What was the result of your decision?
+
+#6. Think Big, shoot for the stars, but landed on the moon
